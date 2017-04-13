3c6f16050382a9f6572115189eb18b9f4868e608
Grant China <grant.china@spirent.com>
2017-04-13 09:02:01 -1000

    Transitioning from screen to tmux

--- diff_env.sh
+++ diff_env.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-THIS=$(basename ${0})
-REPO_DIR=$(dirname ${0})
+THIS=$(basename "${0}")
+REPO_DIR=$(dirname "${0}")
 
-echo ${REPO_DIR}
+echo "${REPO_DIR}"
 
-pushd ${REPO_DIR} > /dev/null
-find . \( -path ./.git -o -path ./${THIS} \) -prune -o -type f -exec echo {} \; -exec diff {} ~/{} \;
+pushd "${REPO_DIR}" > /dev/null
+find . \( -path ./.git -o -path ./"${THIS}" \) -prune -o -type f -exec echo {} \; -exec diff {} ~/{} \;
 popd > /dev/null
 
 
